541bb10aef6eb018f9b52d72a1d896e1200a0ecf
Ricky Browning <rickthebrick12@gmail.com>
2022-08-21 21:21:52 -0400

    Add 2nd line of code to helloworld

--- ver1/src/HelloWorld.java
+++ ver1/src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("\nHello World from my IDE of choice, VS!\nBecause it is one letter away from VSU!\n");
+        System.out.println("This is my edit to helloWorld.java file for testing upload to github\n")
     }
 }

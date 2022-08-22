8ce6b3e4a54f8451107f257521768318e7957fda
Ricky Browning <rickthebrick12@gmail.com>
2022-08-21 21:17:52 -0400

    Created HelloWorld class, and Acme project

---
+++ ver1/src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nHello World from my IDE of choice, VS!\nBecause it is one letter away from VSU!\n");
+    }
+}

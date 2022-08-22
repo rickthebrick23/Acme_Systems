4656f31a79e6cac822bf94b332b4a5e96c8e9179
Ricky Browning <rickthebrick12@gmail.com>
2022-08-21 22:23:58 -0400

    Created new class hellodolly

---
+++ ver1/src/HelloDolly.java
@@ -0,0 +1,5 @@
+public class HelloDolly {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nHello Dolly from Eclipse, but in my case VS, again\n");
+    }
+}
